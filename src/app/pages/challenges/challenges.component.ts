6e974bf4c1681177d2cc35f9f086780eec277309
redl1k <andrian14756@gmail.com>
2022-11-06 03:20:42 +0300

    upd

---
+++ src/app/pages/challenges/challenges.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages/challenges',
+//templateUrl: './pages\challenges.component.html',
+  templateUrl: './challenges.component.html',
+//  styleUrls: ['./pages/challenges.component.css']
+  styleUrls: ['./challenges.component.css']
+})
+export class challengesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
