9468c5599254807cd71baa3d82b036cf5a9405ff
redl1k <andrian14756@gmail.com>
2022-11-10 14:21:11 +0300

    upd but not working yet...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path:'',
-    redirectTo: 'challenges',
+    redirectTo: './pages/challenges',
     pathMatch: 'full'
   },
 ];
